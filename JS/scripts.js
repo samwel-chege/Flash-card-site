1d5e21dc7424651c9530e92bcfc33955a021dd9a
samwel-chege <samwelchegeh09@gmail.com>
2021-06-16 10:52:58 +0300

    add initial commits

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("div#click-one").click(function(){
+     whatToSay = "Js is a programming language for use in HTML pages";
+    alert(whatToSay);
+  });
+  $("div#click-two").click(function(){
+    whatToSay = "An operator perfoms some operation on single or multiple operands and produces a result"
+    alert(whatToSay);
+  });
+  $("div#click-three").click(function(){
+    whatToSay = "It is a sort of like a container for storing values"
+    alert(whatToSay);
+  });
+});
