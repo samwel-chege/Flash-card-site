a1aaf921d3f0ebdd127d95d55133c99779f0d321
samwel-chege <samwelchegeh09@gmail.com>
2021-06-16 12:11:32 +0300

    make changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,4 +11,72 @@ $(document).ready(function() {
     whatToSay = "It is a sort of like a container for storing values"
     alert(whatToSay);
   });
+  $("div#click-four").click(function(){
+    whatToSay = "These are rules for naming variables"
+    alert(whatToSay);
+  });
+  $("div#click-five").click(function(){
+    whatToSay = "A set of statements that performs a task or calculates a value by taking some input and returning some output"
+    alert(whatToSay);
+  });
+   $("div#click-six").click(function(){
+    whatToSay = "A series of characters inside of quotation marks"
+    alert(whatToSay);
+  });
+   $("div#click-seven").click(function(){
+    whatToSay = "An action taken on something"
+    alert(whatToSay);
+  });
+   $("div#click-eight").click(function(){
+    whatToSay = "An array-like object accesible inside functions that contains the values of the arguments passed to that functon"
+    alert(whatToSay);
+  });
+   $("div#click-nine").click(function(){
+    whatToSay = "The parameters in a function call, are the function's arguments"
+    alert(whatToSay);
+  });
+   $("div#click-ten").click(function(){
+    whatToSay = "The return statement is used to return a particular value from the function caller"
+    alert(whatToSay);
+  });
+   $("div#click-eleven").click(function(){
+    whatToSay = "Function method is a pattern in Javascript where multiple functions are called on the same object consecutively"
+    alert(whatToSay);
+  });
+   $("div#click-twelve").click(function(){
+    whatToSay = "A series of characters inside of quotation marks"
+    alert(whatToSay);
+  });
+   $("div#click-thirteen").click(function(){
+    whatToSay = "A Js Boolean represents one of two values:true or false"
+    alert(whatToSay);
+  });
+   $("div#click-fourteen").click(function(){
+    whatToSay = "Js asigns 'undefined' to any object thta has been declared but not initialized or defined"
+    alert(whatToSay);
+  });
+   $("div#click-fifteen").click(function(){
+    whatToSay = "It is a property of the global object that indicates a value is not a legal number"
+    alert(whatToSay);
+  });
+   $("div#click-sixteen").click(function(){
+    whatToSay = "The escape() function encodes a string"
+    alert(whatToSay);
+  });
+   $("div#click-seventeen").click(function(){
+    whatToSay = "A function that displays text in a dialog box that pops up on the screen"
+    alert(whatToSay);
+  });
+   $("div#click-eighteen").click(function(){
+    whatToSay = "Comments makes the Js interpreter ignore text"
+    alert(whatToSay);
+  });
+   $("div#click-nineteen").click(function(){
+    whatToSay = "A Js library that makes it easy to make web pages interactive-more or less like a framework"
+    alert(whatToSay);
+  });
+   $("div#click-twenty").click(function(){
+    whatToSay = "Additional characteristics or properties of the element such as width or height of an image"
+    alert(whatToSay);
+  });
 });
